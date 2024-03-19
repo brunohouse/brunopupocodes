cba7b675179cf7bf961d5df7fd55aabb42f89507
Bruno Pupo <brunopupo1@gmail.com>
2024-03-19 15:16:16 -0300

    agora sim resolvi o conflito no texto

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-    echo "OLA MUNDO 456 TESTE CARALHOOO!!!!"
-
-    echo "ola caralho"
-
-=======
     echo "OLA MUNDO PORRA TESTE CARALHOOO!!!!"
 
     echo "Teste teste"
->>>>>>> dc6d4d406ff25428fbdd61f96667b7b13c998f72
 ?>
